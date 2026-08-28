9a5a855c2d244fc9062948e9c3467731abf951fe
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-08-28 10:42:20 +0700

    feat: make Vulkan RHI own real swapchain frame lifecycle

--- Source/NeoEngine/Rendering/RHI/Vulkan/VulkanRHI.h
+++ Source/NeoEngine/Rendering/RHI/Vulkan/VulkanRHI.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <vulkan/vulkan.h>
 #include <vector>
-#include <string>
 
 namespace NeoEngine {
 class VulkanRHI {
@@ -9,13 +8,29 @@ class VulkanRHI {
     VkPhysicalDevice m_GPU = VK_NULL_HANDLE;
     VkDevice m_Device = VK_NULL_HANDLE;
     VkQueue m_GraphicsQueue = VK_NULL_HANDLE;
+    VkSurfaceKHR m_Surface = VK_NULL_HANDLE;
     VkSwapchainKHR m_Swapchain = VK_NULL_HANDLE;
     std::vector<VkImage> m_SwapchainImages;
+    std::vector<VkImageView> m_SwapchainViews;
+    std::vector<VkFramebuffer> m_Framebuffers;
+    VkRenderPass m_RenderPass = VK_NULL_HANDLE;
+    VkCommandPool m_CommandPool = VK_NULL_HANDLE;
+    VkCommandBuffer m_CommandBuffer = VK_NULL_HANDLE;
+    VkSemaphore m_ImageAvailable = VK_NULL_HANDLE;
+    VkSemaphore m_RenderFinished = VK_NULL_HANDLE;
+    VkFence m_InFlight = VK_NULL_HANDLE;
     VkFormat m_SwapchainFormat = VK_FORMAT_UNDEFINED;
+    uint32_t m_QueueFamily = UINT32_MAX;
+    uint32_t m_ImageIndex = UINT32_MAX;
     int m_Width = 0;
     int m_Height = 0;
     bool m_Initialized = false;
     bool m_FrameActive = false;
+    bool m_ImageAcquired = false;
+
+    bool CreateSwapchainResources(uint32_t width, uint32_t height);
+    void DestroySwapchainResources();
+
 public:
     static VulkanRHI& Get();
     bool Init(void* nativeWindow, int w, int h, const char* appName = "FauzanEngine");
@@ -26,9 +41,12 @@ public:
     [[nodiscard]] VkDevice GetDevice() const { return m_Device; }
     [[nodiscard]] VkPhysicalDevice GetGPU() const { return m_GPU; }
     [[nodiscard]] VkQueue GetGraphicsQueue() const { return m_GraphicsQueue; }
+    [[nodiscard]] VkSurfaceKHR GetSurface() const { return m_Surface; }
+    [[nodiscard]] VkSwapchainKHR GetSwapchain() const { return m_Swapchain; }
     [[nodiscard]] int GetWidth() const { return m_Width; }
     [[nodiscard]] int GetHeight() const { return m_Height; }
     [[nodiscard]] bool IsInitialized() const { return m_Initialized; }
     [[nodiscard]] bool IsFrameActive() const { return m_FrameActive; }
+    [[nodiscard]] bool HasSwapchain() const { return m_Swapchain != VK_NULL_HANDLE; }
 };
 }
