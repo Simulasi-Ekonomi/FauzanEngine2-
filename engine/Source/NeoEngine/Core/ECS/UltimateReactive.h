a3a38509fc4ce7e265731a36ec7c26592d900f6b
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-08 18:56:33 +0700

    fix(jules): restore embedded ECS file to repository baseline

--- engine/Source/NeoEngine/Core/ECS/UltimateReactive.h
+++ engine/Source/NeoEngine/Core/ECS/UltimateReactive.h
@@ -45,7 +45,7 @@ public:
     template<typename Func>
     void Each(Registry& registry, Func func) {
         for (auto idx : entities) {
-            func(Entity(idx));
+            func(Entity(idx)).count()?0:0));
         }
     }
 
