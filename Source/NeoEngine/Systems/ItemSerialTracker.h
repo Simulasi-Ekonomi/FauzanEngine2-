935fa9512080a3b0cbf1da7dec1113559e1aa21c
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-15 23:24:48 +0700

    R4: make ItemSerialTracker logging portable for host builds

--- Source/NeoEngine/Systems/ItemSerialTracker.h
+++ Source/NeoEngine/Systems/ItemSerialTracker.h
@@ -4,18 +4,24 @@
 #include <unordered_map>
 #include <mutex>
 #include <random>
-#include <mutex>
 #include <chrono>
 #include <sstream>
 #include <iomanip>
 #include <functional>
+#include <cstdio>
 #include <curl/curl.h>
 #include <json/json.h>
-#include <android/log.h>
 
+#if defined(__ANDROID__)
+#include <android/log.h>
 #define LOG_TAG "ItemSerialTracker"
 #define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
 #define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+#else
+#define LOG_TAG "ItemSerialTracker"
+#define LOGI(...) std::fprintf(stderr, "[INFO][%s] ", LOG_TAG), std::fprintf(stderr, __VA_ARGS__), std::fputc('\n', stderr)
+#define LOGE(...) std::fprintf(stderr, "[ERROR][%s] ", LOG_TAG), std::fprintf(stderr, __VA_ARGS__), std::fputc('\n', stderr)
+#endif
 
 namespace NeoEngine {
 
@@ -35,7 +41,7 @@ struct SerialNumber {
 class ItemSerialTracker {
 public:
     ItemSerialTracker();
-    
+
     std::string GenerateSerial(const std::string& itemType, const std::string& itemName);
     SerialNumber* RegisterItem(const std::string& ownerId, const std::string& itemType,
                                const std::string& itemName, int quantity, const std::string& source);
@@ -44,22 +50,21 @@ public:
     bool IsItemValid(const std::string& serialNumber) const;
     bool ConsumeItem(const std::string& serialNumber);
     int VerifyPendingBatch(int maxBatch = 10);
-    
+
     std::string GenerateCurrencySerial(const std::string& currencyType, int amount, const std::string& source);
     std::string GenerateRewardSerial(const std::string& rewardName, const std::string& source);
     bool IsSerialDuplicate(const std::string& serialNumber) const;
     std::string GetAuditTrail(const std::string& playerId) const;
-    
+
     int GetVerifiedCount() const { return m_VerifiedCount; }
     int GetRejectedCount() const { return m_RejectedCount; }
     int GetTotalItems() const { return m_TotalItems; }
-    
+
     void SetServerURL(const std::string& url) { m_ServerURL = url; }
     void SetServerPublicKey(const std::string& key) { m_ServerPublicKey = key; }
     void SetOnCheatDetected(std::function<void(const std::string&, const std::string&)> cb) { m_OnCheatDetected = cb; }
     void SetOnVerified(std::function<void(const std::string&)> cb) { m_OnVerified = cb; }
 
-    // Method tambahan untuk AntiCheatSystem
     void MarkAllPlayerItemsContaminated(const std::string& playerId, const std::string& playerName);
     bool TransferOwnership(const std::string& serial, const std::string& fromId, const std::string& fromName, const std::string& toId, const std::string& toName, const std::string& method);
 
@@ -75,7 +80,7 @@ private:
     int m_TotalItems = 0;
     std::function<void(const std::string&, const std::string&)> m_OnCheatDetected;
     std::function<void(const std::string&)> m_OnVerified;
-    
+
     static size_t WriteCallback(void* contents, size_t size, size_t nmemb, std::string* output);
     std::string GenerateTransferHash(const std::string& serial, const std::string& from,
                                      const std::string& to, const std::string& method) const;
