f610834105461dcd5b906ea775e93af2b3769011
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-19 01:32:21 +0700

    P1: expose canonical scene mesh refresh seam

--- Source/NeoEngine/Runtime/NeoRuntime.h
+++ Source/NeoEngine/Runtime/NeoRuntime.h
@@ -98,6 +98,8 @@ public:
     const SceneWorld* Scene() const { return m_Scene.get(); }
     SceneMeshAdapter* SceneMeshes() { return m_SceneMeshes.get(); }
     const SceneMeshAdapter* SceneMeshes() const { return m_SceneMeshes.get(); }
+    bool RefreshSceneMesh(SceneEntity entity, const CpuMeshResource& mesh, const CpuMaterialResource& material);
+    bool RefreshSceneMesh(SceneEntity entity, const CpuMeshResource& mesh, const CpuMaterialResource& material, const CpuTextureResource* texture);
     RenderCamera* SceneCamera() { return m_SceneCamera.get(); }
     const RenderCamera* SceneCamera() const { return m_SceneCamera.get(); }
     Vulkan3DRenderer* VulkanRenderer() { return m_VulkanRenderer.get(); }
