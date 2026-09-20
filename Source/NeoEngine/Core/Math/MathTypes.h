8bef4e23d4207de08d2898e0a8d58f5e04845f68
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-20 18:42:32 +0700

    Core: unify MathTypes with canonical Mat4 definition

--- Source/NeoEngine/Core/Math/MathTypes.h
+++ Source/NeoEngine/Core/Math/MathTypes.h
@@ -1,12 +1,5 @@
 #pragma once
 
-namespace NeoEngine {
-
-
-struct Mat4
-{
-    float m[16];
-};
-
-}
-
+// Canonical 4x4 matrix type lives in Mat4.h. Keep this compatibility header
+// as a stable include path without defining a second Mat4 type.
+#include "Mat4.h"
