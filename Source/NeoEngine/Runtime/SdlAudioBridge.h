59f85c4e8cb198f9a0c009babd984c853b578485
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-20 09:32:32 +0700

    P1: expose spatial audio voice lifecycle

--- Source/NeoEngine/Runtime/SdlAudioBridge.h
+++ Source/NeoEngine/Runtime/SdlAudioBridge.h
@@ -22,6 +22,8 @@ public:
 
     bool Initialize(uint16_t framesPerCallback = 256);
     bool Play(uint32_t id, std::vector<int16_t> mono, uint16_t gainQ8 = 256);
+    bool PlaySpatial(const SpatialVoiceParams& params);
+    bool Stop(uint32_t id);
     bool UpdateVoicePosition(uint32_t id, const float position[3]);
     bool UpdateVoicePitch(uint32_t id, float pitch);
     bool UpdateVoiceGain(uint32_t id, uint16_t gainQ8);
