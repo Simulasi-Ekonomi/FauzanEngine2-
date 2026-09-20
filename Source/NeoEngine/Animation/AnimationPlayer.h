b6966b07be0ad377b268877026b9e17e7c1bb066
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-20 15:44:48 +0700

    p1: integrate animation playback with skeleton pose evaluation

--- Source/NeoEngine/Animation/AnimationPlayer.h
+++ Source/NeoEngine/Animation/AnimationPlayer.h
@@ -3,23 +3,35 @@
 #include "Skeleton.h"
 #include "AnimationClip.h"
 
-namespace NeoEngine
-{
+#include <cstddef>
+#include <vector>
 
-class AnimationPlayer
-{
+namespace NeoEngine {
+
+enum class AnimationPlaybackMode : unsigned char { Clamp, Loop };
+
+class AnimationPlayer {
 public:
+    static constexpr size_t kMaxPaletteBones = Skeleton::kMaxBones;
 
     void Play(AnimationClip* clip);
+    void Stop() noexcept;
+    void SetPlaybackMode(AnimationPlaybackMode mode) noexcept { playbackMode_ = mode; }
+    void SetSkeleton(const Skeleton* skeleton) noexcept { skeleton_ = skeleton; }
+    bool Update(float dt);
+    bool EvaluatePose(std::vector<Mat4>& localPose, std::vector<Mat4>& skinningPalette) const;
 
-    void Update(float dt);
+    [[nodiscard]] bool IsPlaying() const noexcept { return currentClip_ != nullptr && playing_; }
+    [[nodiscard]] float GetTime() const noexcept { return time_; }
+    [[nodiscard]] AnimationClip* GetCurrentClip() const noexcept { return currentClip_; }
+    [[nodiscard]] AnimationPlaybackMode GetPlaybackMode() const noexcept { return playbackMode_; }
 
 private:
-
-    AnimationClip* currentClip = nullptr;
-
-    float time = 0;
-
+    AnimationClip* currentClip_ = nullptr;
+    const Skeleton* skeleton_ = nullptr;
+    float time_ = 0.0F;
+    bool playing_ = false;
+    AnimationPlaybackMode playbackMode_ = AnimationPlaybackMode::Loop;
 };
 
 }
