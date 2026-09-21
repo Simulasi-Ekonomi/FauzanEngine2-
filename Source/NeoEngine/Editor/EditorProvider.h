6d492e3d80c20154effd1df967796a2e47be600c
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-21 12:49:21 +0700

    p3: harden editor command contract

--- Source/NeoEngine/Editor/EditorProvider.h
+++ Source/NeoEngine/Editor/EditorProvider.h
@@ -1,13 +1,16 @@
 #pragma once
 #include <string>
 #include <functional>
+#include <cstdint>
 
 namespace NeoEngine {
 
 struct EditorCommand {
     std::string action;
     std::string data;
+    std::uint64_t sequence = 0U;
 };
+enum class EditorProviderError : std::uint8_t { None, InvalidCommand, Capacity, CallbackFailure };
 
 class EditorProvider {
 public:
@@ -19,22 +22,22 @@ public:
     void SetPaused(bool paused) { m_Paused = paused; }
     bool IsPaused() const { return m_Paused; }
 
-    void ExecuteCommand(const std::string& action, const std::string& data = "") {
-        if (m_OnCommand) m_OnCommand({action, data});
-    }
+    bool ExecuteCommand(const std::string& action, const std::string& data = "");
 
     void SetCommandCallback(std::function<void(const EditorCommand&)> cb) {
         m_OnCommand = cb;
     }
 
-    std::string GetEditorStateJSON() const {
-        return "{\"paused\":" + std::string(m_Paused ? "true" : "false") + "}";
-    }
+    std::string GetEditorStateJSON() const;
+    [[nodiscard]] std::uint64_t CommandSequence() const { return m_CommandSequence; }
+    [[nodiscard]] EditorProviderError LastError() const { return m_LastError; }
 
 private:
     EditorProvider() = default;
     bool m_Paused = false;
     std::function<void(const EditorCommand&)> m_OnCommand;
+    std::uint64_t m_CommandSequence = 0U;
+    EditorProviderError m_LastError = EditorProviderError::None;
 };
 
 } // namespace NeoEngine
