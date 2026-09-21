08e0b7910e90ee50e626f76ade2b31b7c8316d2b
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-21 12:47:26 +0700

    p1: harden animation graph contract

--- Source/NeoEngine/Animation/AnimationGraph.h
+++ Source/NeoEngine/Animation/AnimationGraph.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <vector>
 #include <functional>
+#include <cstddef>
+#include <cstdint>
 
 struct AnimationNode
 {
@@ -10,11 +12,16 @@ struct AnimationNode
 class AnimationGraph
 {
 public:
-
-    void AddNode(const AnimationNode& node);
-    void Update();
-
+    static constexpr std::size_t kMaxNodes = 4096U;
+    bool AddNode(const AnimationNode& node);
+    bool RemoveNode(std::size_t index);
+    void Clear() noexcept;
+    bool Update();
+    [[nodiscard]] std::size_t Size() const noexcept { return nodes.size(); }
+    [[nodiscard]] std::uint64_t Revision() const noexcept { return revision_; }
+    [[nodiscard]] bool IsUpdating() const noexcept { return updating_; }
 private:
-
     std::vector<AnimationNode> nodes;
+    std::uint64_t revision_ = 0U;
+    bool updating_ = false;
 };
