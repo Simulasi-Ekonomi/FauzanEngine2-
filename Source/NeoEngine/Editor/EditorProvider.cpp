0b28b0a30c69d41170a8ef0c0bd46c7681c89f57
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-21 14:03:01 +0700

    Remove unused helper and keep editor provider lean

--- Source/NeoEngine/Editor/EditorProvider.cpp
+++ Source/NeoEngine/Editor/EditorProvider.cpp
@@ -2,18 +2,6 @@
 
 #include <limits>
 
-namespace {
-std::string EscapeJson(const std::string& value) {
-    std::string out;
-    out.reserve(value.size() + 8U);
-    for (const unsigned char c : value) {
-        switch (c) { case '\\': out += "\\\\"; break; case '"': out += "\\\""; break; case '\n': out += "\\n"; break; case '\r': out += "\\r"; break; case '\t': out += "\\t"; break; default: if (c < 0x20U) return {}; out += static_cast<char>(c); }
-    }
-    return out;
-}
-}
-
-
 namespace NeoEngine {
 bool EditorProvider::ExecuteCommand(const std::string& action, const std::string& data) {
     if (action.empty() || action.size() > 128U || data.size() > 8192U || action.find('\0') != std::string::npos || data.find('\0') != std::string::npos) {
