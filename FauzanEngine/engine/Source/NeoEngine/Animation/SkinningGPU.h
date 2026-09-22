12ac351bbe957b728699acbff8904ac4263b5bfa
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-22 10:25:29 +0700

    Cross-branch gap closure: harden asset and animation runtime

--- FauzanEngine/engine/Source/NeoEngine/Animation/SkinningGPU.h
+++ FauzanEngine/engine/Source/NeoEngine/Animation/SkinningGPU.h
@@ -1,17 +1,4 @@
 #pragma once
-
 #include <vector>
 #include <array>
-
-class SkinningGPU
-{
-public:
-
-    void UploadBones(const std::vector<std::array<float,16>>& matrices);
-
-    void Bind();
-
-private:
-
-    [[maybe_unused]] unsigned int boneBuffer;
-};
+class SkinningGPU{public:void UploadBones(const std::vector<std::array<float,16>>&);void Bind();size_t BoneCount()const{return palette.size();}bool IsBound()const{return bound;}private:std::vector<std::array<float,16>>palette;bool bound=false;};
