8398a60409da61fd90c9c43f131b6525682f65c0
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-22 10:22:01 +0700

    P1 gap closure: implement asset and animation runtime paths

--- FauzanEngine/engine/Source/NeoEngine/Animation/AnimationGraph.cpp
+++ FauzanEngine/engine/Source/NeoEngine/Animation/AnimationGraph.cpp
@@ -1,15 +1,3 @@
 #include "AnimationGraph.h"
-
-void AnimationGraph::AddNode(const AnimationNode& node)
-{
-    nodes.push_back(node);
-}
-
-void AnimationGraph::Update()
-{
-    for(auto& n : nodes)
-    {
-        if(n.update)
-            n.update();
-    }
-}
+void AnimationGraph::AddNode(const AnimationNode& node){ if(node.update) nodes.push_back(node); }
+void AnimationGraph::Update(){ for(auto& node:nodes) if(node.update) node.update(); }
