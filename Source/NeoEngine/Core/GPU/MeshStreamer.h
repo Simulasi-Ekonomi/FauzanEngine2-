8895784aed555f04e6e4f2233d846299178baa3c
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-24 18:07:27 +0700

    Implement MeshStreamer Vulkan interface

--- Source/NeoEngine/Core/GPU/MeshStreamer.h
+++ Source/NeoEngine/Core/GPU/MeshStreamer.h
@@ -1,14 +1,41 @@
 #pragma once
 
+#include <vulkan/vulkan.h>
+#include <cstdint>
+
 namespace NeoEngine
 {
+
+class VulkanGPUBuffer;
 
 class MeshStreamer
 {
 public:
+    MeshStreamer() = default;
+    ~MeshStreamer() = default;
 
-    void UploadMesh(const void* vertices);
+    MeshStreamer(const MeshStreamer&) = delete;
+    MeshStreamer& operator=(const MeshStreamer&) = delete;
+    MeshStreamer(MeshStreamer&&) noexcept = default;
+    MeshStreamer& operator=(MeshStreamer&&) noexcept = default;
 
+    [[nodiscard]] bool Initialize(VkDevice device, VkPhysicalDevice physicalDevice, VkDeviceSize capacityBytes);
+    [[nodiscard]] bool UploadMesh(const void* vertices, VkDeviceSize byteCount);
+    void Destroy();
+
+    [[nodiscard]] bool IsValid() const;
+    [[nodiscard]] VkBuffer GetBuffer() const;
+    [[nodiscard]] VkDeviceSize GetCapacityBytes() const { return capacityBytes_; }
+    [[nodiscard]] VkDeviceSize GetUploadedBytes() const { return uploadedBytes_; }
+
+private:
+    VulkanGPUBuffer* Buffer();
+    const VulkanGPUBuffer* Buffer() const;
+
+    VulkanGPUBuffer* buffer_ = nullptr;
+    VkDeviceSize capacityBytes_ = 0;
+    VkDeviceSize uploadedBytes_ = 0;
 };
 
 }
+
