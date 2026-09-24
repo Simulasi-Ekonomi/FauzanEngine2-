dabfd5fbf4ffb76e14d06847b292738d7d73401e
Simulasi-Ekonomi <butetnadia@gmail.com>
2026-09-24 19:49:23 +0700

    Restore AnimationGraph public NeoEngine namespace contract

--- Source/NeoEngine/Animation/AnimationGraph.h
+++ Source/NeoEngine/Animation/AnimationGraph.h
@@ -1,27 +1,30 @@
 #pragma once
-#include <vector>
-#include <functional>
 #include <cstddef>
 #include <cstdint>
+#include <functional>
+#include <vector>
 
-struct AnimationNode
-{
+namespace NeoEngine {
+
+struct AnimationNode {
     std::function<void()> update;
 };
 
-class AnimationGraph
-{
+class AnimationGraph {
 public:
     static constexpr std::size_t kMaxNodes = 4096U;
-    bool AddNode(const AnimationNode& node);
-    bool RemoveNode(std::size_t index);
+    [[nodiscard]] bool AddNode(const AnimationNode& node);
+    [[nodiscard]] bool RemoveNode(std::size_t index);
     void Clear() noexcept;
-    bool Update();
+    [[nodiscard]] bool Update();
     [[nodiscard]] std::size_t Size() const noexcept { return nodes.size(); }
     [[nodiscard]] std::uint64_t Revision() const noexcept { return revision_; }
     [[nodiscard]] bool IsUpdating() const noexcept { return updating_; }
+
 private:
     std::vector<AnimationNode> nodes;
     std::uint64_t revision_ = 0U;
     bool updating_ = false;
 };
+
+}
